d39da4168d7407776364b153896699e3486cf5fe
Rick Marck <rickmarck7@gmail.com>
2024-06-20 09:09:40 -0400

    Fix - Corrigindo erros em endpoits e importando BaseModel do pydantic

--- app/endpoints/cat_question.py
+++ app/endpoints/cat_question.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, Body, HTTPException
 from app.models.cat_question import lista_gatos, datas_nascimento
-
+from pydantic import BaseModel
 router = APIRouter()
 
+class GatoNome(BaseModel):
+    nome:str
+class GatoRaca(BaseModel):
+    raca:str
 # Método que recebe um ID e retorna os dados do gato e sua data de nascimento
 @router.get("/gato/{id}")
 def get_gato_por_id(id: int):
@@ -22,7 +26,7 @@ def get_gato_por_id(id: int):
             'nome': gato.nome,
             'raca': gato.raca,
             'idade': gato.idade,
-            'data_nascimento': data_nascimento.strftime('%Y-%m-%d')
+            'data_nascimento': data_nascimento
         }
     except HTTPException as e: #criando uma execeção que captura as exceções dentro do bloco e atribuindo a variavél e para capturar a exceção
         raise e
@@ -35,7 +39,6 @@ def get_gatos():
     try:
         gatos_sem_data_nascimento = []
         for gato in lista_gatos:
-            return lista_gatos
             if not any(d['id'] == gato.id for d in datas_nascimento):
                 gatos_sem_data_nascimento.append({
                     'id': gato.id,
@@ -47,7 +50,7 @@ def get_gatos():
         raise HTTPException(status_code=422, detail="Gatos gatos sem data de nascimento") #colocar erro pra falta de recurso
 
 # Método que retorna uma lista de todos os gatos mais velhos com a mesma idade
-@router.post("/gatos-mais-velhos")
+@router.get("/gatos-mais-velhos")
 def get_gatos_mais_velhos():
     try:
         # Encontra a idade máxima entre todos os gatos
@@ -60,14 +63,14 @@ def get_gatos_mais_velhos():
                 # Encontra a data de nascimento do gato
                 data_nascimento_gato = next((d['data_nascimento'] for d in datas_nascimento if d['id'] == gato.id), None)
                 if data_nascimento_gato is None:
-                    raise HTTPException(status_code="422", detail="Os gatos não possuem data de nascimento")
+                    raise HTTPException(status_code=422, detail="Os gatos não possuem data de nascimento")
                 # Adiciona o gato à lista com sua data de nascimento
                 gatos_mais_velhos.append({
                     'id': gato.id,
                     'nome': gato.nome,
                     'raca': gato.raca,
                     'idade': gato.idade,
-                    'data_nascimento': data_nascimento_gato.strftime('%Y-%m-%d')
+                    'data_nascimento': data_nascimento_gato
                 })
         return gatos_mais_velhos
     except HTTPException as er:
@@ -76,15 +79,16 @@ def get_gatos_mais_velhos():
         raise HTTPException(status_code=500, detail="Internal Server Error") #erro pra falta de data?
 
 # Método que busca gatos por um termo de busca no nome
-@router.post("/buscar-gatos")
-def buscar_gatos_por_nome(termo_busca: str = Body(...)):
+@router.post("/buscar-gatos", response_model=GatoNome)
+def buscar_gatos_por_nome(termo_busca: GatoNome = Body(...)):
+    print(termo_busca)
     try:
         # Transforma a lista de gatos em um dicionário onde as chaves são os IDs
         dict_gatos = {gato.id: gato for gato in lista_gatos}
         
         gatos_encontrados = []
         for gato_id, gato in dict_gatos.items():
-            if termo_busca.lower() in gato.nome.lower():                
+            if termo_busca.nome.lower() in gato.nome.lower():                
                 # Adiciona o gato encontrado ao resultado, excluindo o atributo idade
                 gatos_encontrados.append(gato)
 
@@ -98,10 +102,10 @@ def buscar_gatos_por_nome(termo_busca: str = Body(...)):
 
 
 # Método que busca gatos por raça
-@router.post("/buscar-raca")
-def buscar_gatos_por_raca(termo_busca: str = Body(...)):
+@router.post("/buscar-raca", response_model=GatoRaca)
+def buscar_gatos_por_raca(termo_busca: GatoRaca = Body(...)):
     try:
-        gatos_encontrados = [gato.__dict__ for gato in lista_gatos if gato.raca.lower() == termo_busca.lower()]
+        gatos_encontrados = [gato.__dict__ for gato in lista_gatos if gato.raca.lower() == termo_busca.raca.lower()]
         if not gatos_encontrados: #erro se o array estiver vazio
             raise HTTPException(status_code=404, detail="Nenhum gato dessa raça foi encontrado")
             
@@ -117,3 +121,4 @@ def buscar_gatos_por_raca(termo_busca: str = Body(...)):
 #Ajeitar os post e get (já)
 #Ajeitar os números dos erros (já)
 #implementar erros (já)
+#tentar tratar erros de maneira automatica
